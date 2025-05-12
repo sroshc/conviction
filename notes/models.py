bc48bab42084ceed88a63ea0df3f2af63d9f7a67
sroshc <shirzaysroshsafa@gmail.com>
2025-05-12 10:48:01 -0400

    made migrations

--- notes/models.py
+++ notes/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Directory(models.model):
-    user = models.OnetoOneField(User, on_delete=models.CASCADE)
+class Directory(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
     is_root = models.BooleanField(default=False)
+    parent = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='subdirectories')
 
-class File(models.model):
+class File(models.Model):
     directory = models.ForeignKey(Directory, on_delete=models.CASCADE)
     
     name = models.CharField(max_length=20)
     content = models.TextField()
-    date_created = models.DateTimeField(auto_new_add=True)
+    date_created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from notes.models import Directory
 
 # Create your models here.
 
-class Writer(models.models):
-    user = models.OnetoOneField(User, on_delete=models.CASCADE)
-    #root_directory =  models.OnetoOne
+class Writer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    root_directory =  models.OneToOneField(Directory, on_delete=models.CASCADE)
